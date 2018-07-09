3a08939525cb9dc31804385158bb893bf0ad44ff
Derefaa <derefaacline@er-10-131-170-195.wireless.umn.edu>
2018-07-09 13:12:10 -0500

    Added a websocket called socket.io. The server  had socket.io work with express and http request. The http is also the listening fuction. The server also started the socket io event while the index.html initalized it and used it.

--- Server.js
+++ Server.js
@@ -1,6 +1,8 @@
 var express = require("express")
 var bodyParser = require("body-parser")
 var app = express()
+var http = require("http").Server(app)
+var io = require("socket.io")(http)
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
@@ -20,11 +22,16 @@ app.get('/messages', (req, res) =>{
 app.post('/messages', (req, res) =>{
     console.log(req.body)
     messages.push(req.body)
+    io.emit('message',req.body)
     res.sendStatus(200)
 
 })
+io.on("connection",(socket) => {
+console.log("User is connected")
+})
 
-var server = app.listen(3000, () => {
+
+var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
 
