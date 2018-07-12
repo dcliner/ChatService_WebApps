f057276225bc31cb82ba7ad8dbed9d5718f74373
Derefaa <derefaacline@er-10-131-170-195.wireless.umn.edu>
2018-07-12 12:19:12 -0500

    Downloaded mongoose package and conected the program to mongoDB

--- Server.js
+++ Server.js
@@ -3,11 +3,14 @@ var bodyParser = require("body-parser")
 var app = express()
 var http = require("http").Server(app)
 var io = require("socket.io")(http)
+var mongoose = require("mongoose")
+
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+var dbUrl = "mongodb://Chat_Users:dcliner1998@ds233061.mlab.com:33061/chat_service_node_js"
 
 var messages = [
    { name: "Ibi", message: "Hey"},
@@ -29,6 +32,15 @@ app.post('/messages', (req, res) =>{
 io.on("connection",(socket) => {
 console.log("User is connected")
 })
+
+mongoose.connect(dbUrl, { useNewUrlParser: true });
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'dbUrl connection error:'));
 
 
 var server = http.listen(3000, () => {
