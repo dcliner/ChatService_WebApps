c2a5004931bb56aeadd947c898609e8e0d94e6bb
Derefaa <derefaacline@er-10-131-170-195.wireless.umn.edu>
2018-07-12 12:56:19 -0500

    Got am mdel with a schema inside that stores the messages

--- Server.js
+++ Server.js
@@ -11,29 +11,39 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 var dbUrl = "mongodb://Chat_Users:dcliner1998@ds233061.mlab.com:33061/chat_service_node_js"
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
-var messages = [
-   { name: "Ibi", message: "Hey"},
-   {name: "Samuel", message: "Hello"}
-]
 
 app.get('/messages', (req, res) =>{
-    res.send(messages)
+    Message.find({}, (err,messages) =>{
+        res.send(messages)
+    })
+   
 
 })
 
 app.post('/messages', (req, res) =>{
-    console.log(req.body)
-    messages.push(req.body)
+    var message = new Message(req.body)
+message.save((err) => {
+    if (err)
+        sendStatus(500)
+
     io.emit('message',req.body)
     res.sendStatus(200)
+
+})
+   
 
 })
 io.on("connection",(socket) => {
 console.log("User is connected")
 })
 
-mongoose.connect(dbUrl, { useNewUrlParser: true });
+mongoose.connect(dbUrl, { useNewUrlParser: true }, (err) => 
+(console.log('mongo db connection', err)));
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
 //Get the default connection
